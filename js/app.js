cf281a51d8b5efaf1444e6081a601e25b090abd5
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-09-03 04:58:26 +0600

    add modal section

--- js/app.js
+++ js/app.js
@@ -32,22 +32,17 @@ const newsProtal = async (news) => {
 }
 
 const displayNews = newsData => {
-    // console.log(newsData)
-
     
     const newsContainer = document.getElementById('news-container')
     newsContainer.innerHTML ='';
 
-
     for(const news of newsData){
-        console.log(news)
 
         const div = document.createElement('div')
         div.classList.add('card')
         div.classList.add('card-side')
         div.classList.add('bg-base-100')
         div.classList.add('mb-7')
-        // div.classList.add('')
         div.innerHTML = `
         <img class=" p-5" src="${news.thumbnail_url}" alt="News">
         <div class="card-body ">
@@ -79,16 +74,37 @@ const displayNews = newsData => {
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 </div>
-                <button type="button" class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
+                
+                <label onclick ="newsDetails('${news._id}')" for="my-modal-5" ><a class="modal-button text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Icon description</span>
-                </button>
+              </a>                
+                </label>
             </div>
         </div>
         `;
         newsContainer.appendChild(div)
     }
 }
+
+
+const newsDetails = async (details) => {
+    const res = await fetch (`https://openapi.programming-hero.com/api/news/${details}`)
+    const data = await res.json()
+    modalDetails(data.data)
+}
+
+const modalDetails = async (modalDetail) => {
+    console.log(modalDetail)
+
+
+
+}
+
+
+
+
+
 
 displayCategory()
 
