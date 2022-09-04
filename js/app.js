062af444cd70f1125aefc9d9f183eba3f585dfaf
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-09-04 07:04:30 +0600

    add err

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 const loadData = async () => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/categories`)
-    const data = await res.json()
-    return data.data.news_category;
+    try{
+        const res = await fetch(`https://openapi.programming-hero.com/api/news/categories`)
+        const data = await res.json()
+        return data.data.news_category;
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 
 const displayCategory = async () => {
@@ -25,9 +30,14 @@ const displayCategory = async () => {
 
 const newsProtal = async (news) => {
     spinner(true)
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${news}`)
-    const allNews = await res.json()
-    displayNews(allNews.data)
+    try{
+        const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${news}`)
+        const allNews = await res.json()
+        displayNews(allNews.data)
+    }
+    catch (error){
+        console.log(error)
+    }
 }
 
 const displayNews = async newsData => {  
@@ -46,63 +56,80 @@ const displayNews = async newsData => {
     const newsContainer = document.getElementById('news-container')
     newsContainer.innerHTML ='';
 
-    
-    for(const news of newsData){
-    
-        const div = document.createElement('div')
-        div.classList.add('card')
-        div.classList.add('sm:card-top')
-        div.classList.add('md:card-side')
-        div.classList.add('bg-base-100')
-        div.classList.add('mb-7')
-        div.innerHTML = `
-        <img class=" p-5 sm:w-25 sm:h-25 md:h-full lg:h-full xl:h-full 2xl:h-full " src="${news.thumbnail_url}" alt="News">
-        <div class="card-body sm:w-full md:w-5/12  ">
-            <h3 class="card-title sm:text-sm md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl font-bold">${news.title}</h3>
-            <p class ="sm:text-sm md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl font-medium opacity-50 "> ${news.details.slice(0,278)}</p>
-            <p class =" truncate box-border sm:text-sm md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl font-medium opacity-50 "> ${news.details.slice(279, 600)}</p>
-            <div class="card-actions flex justify-between items-center ">
-                <div class = "flex gap-4">
-                    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                        <div class="w-10 rounded-full">
-                            <img src="${news.author.img}" />
+
+    let Number = []
+    for(const news of newsData){ 
+        const cardView = news.total_view 
+        
+        Number.push(cardView)
+        Number.sort((a,b) => b-a)
+       
+        
+        if(Number[0] === cardView || Number[0] === null || Number[0] > 0 ){
+
+            const div = document.createElement('div')
+            div.classList.add('card')
+            div.classList.add('sm:card-top')
+            div.classList.add('md:card-side')
+            div.classList.add('bg-base-100')
+            div.classList.add('mb-7')
+            div.innerHTML = `
+            <img class=" p-5 sm:w-25 sm:h-25 md:h-full lg:h-full xl:h-full 2xl:h-full " src="${news.thumbnail_url}" alt="News">
+            <div class="card-body sm:w-full md:w-5/12  ">
+                <h3 class="card-title sm:text-sm md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl font-bold">${news.title}</h3>
+                <p class ="sm:text-sm md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl font-medium opacity-50 "> ${news.details.slice(0,278)}</p>
+                <p class =" truncate box-border sm:text-sm md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl font-medium opacity-50 "> ${news.details.slice(279, 600)}</p>
+                <div class="card-actions flex justify-between items-center ">
+                    <div class = "flex gap-4">
+                        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                            <div class="w-10 rounded-full">
+                                <img src="${news.author.img}" />
+                            </div>
+                        </label>
+                        <div>
+                            <p class ="font-bold">${news.author.name ? news.author.name : 'No data available' }</p>
+                            <small  class ="opacity-75" >${news.author.published_date ? news.author.published_date : 'No data available' }</small>
                         </div>
+                    </div>
+                    <div class ="flex gap-3">
+                        <div>
+                            <i class="fa-regular fa-eye"></i>
+                        </div>
+                        <p class ="font-bold">${news.total_view ? news.total_view : 'No data available' } M</p>
+                    </div>
+                    <div class ="flex gap-2">
+                        <i class="fa-regular fa-star-half-stroke"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                    </div>
+                    <label onclick ="newsDetails('${news._id}')" for="my-modal-5" ><a class=" cursor-pointer modal-button text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Icon description</span>
+                  </a>                
                     </label>
-                    <div>
-                        <p class ="font-bold">${news.author.name ? news.author.name : 'No data available' }</p>
-                        <small class ="opacity-75" >${news.author.published_date ? news.author.published_date : 'No data available' }</small>
-                    </div>
                 </div>
-                <div class ="flex gap-3">
-                    <div>
-                        <i class="fa-regular fa-eye"></i>
-                    </div>
-                    <p class ="font-bold">${news.total_view ? news.total_view : 'No data available' } M</p>
-                </div>
-                <div class ="flex gap-2">
-                    <i class="fa-regular fa-star-half-stroke"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                    <i class="fa-regular fa-star"></i>
-                </div>
-                <label onclick ="newsDetails('${news._id}')" for="my-modal-5" ><a class=" cursor-pointer modal-button text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
-                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                <span class="sr-only">Icon description</span>
-              </a>                
-                </label>
             </div>
-        </div>
-        `;
-        newsContainer.appendChild(div)
+            `;
+           
+                newsContainer.appendChild(div)
+        }
+    
+            
     } 
     spinner(false)
 }
 
 const newsDetails = async (details) => {
-    const res = await fetch (`https://openapi.programming-hero.com/api/news/${details}`)
-    const data = await res.json()
-    modalDetails(data.data)
+    try{
+        const res = await fetch (`https://openapi.programming-hero.com/api/news/${details}`)
+        const data = await res.json()
+        modalDetails(data.data)
+    }
+    catch(error){
+       console.log(error)
+    }
 }
 
 const modalDetails = async (modalDetail) => {
